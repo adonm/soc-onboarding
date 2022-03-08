30ab448cc4b4a6fc887deb4ac41fe3bbfce43a96
Adon Metcalfe <adonm@fastmail.fm>
2022-03-08 15:29:29 +0800

    string fix

--- onboard.py
+++ onboard.py
@@ -3,4 +3,4 @@ from subprocess import run, check_output
 import json
 accounts = json.loads(check_output("az account list", shell=True))
 for account in account:
-    print(f"Name: {account["name"]}, TenantID: {account["tenantId"]}")
+    print(f'Name: {account["name"]}, TenantID: {account["tenantId"]}')
